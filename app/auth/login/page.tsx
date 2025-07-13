6e53e4fd4d5ba55fa4ad5f59c1c695f0037914e5
bezo <bezo16@protonmail.ch>
2025-07-13 21:16:17 +0200

    build: authorization middleware and feed page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,17 +5,38 @@ import { Input } from "@chakra-ui/react";
 
 const LoginPage = () => {
 
-    const handleLogin = (event: React.FormEvent) => {
+    const handleLogin = async (event: React.FormEvent) => {
         event.preventDefault();
-        console.log("pes")
-    };
+        const formData = new FormData(event.target as HTMLFormElement);
+        const username = formData.get("username")
+        const password = formData.get("password")
+
+        const response = await fetch("http://localhost:4000/auth/login", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+            credentials: "include", 
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Registration successful:", data);
+            // Handle successful registration (e.g., redirect to login page)
+        } else {
+            const errorData = await response.json();
+            console.error("Registration failed:", errorData);
+            // Handle registration error (e.g., show error message)
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1>Login Page</h1>
             <form className="flex flex-col gap-4 w-full max-w-sm" onSubmit={handleLogin}>
-                <Input type="email" placeholder="Enter your email" variant="outline" required={true} />
-                <Input type="password" placeholder="Enter your password" variant="outline" required={true} />
+                <Input type="username" name="username" placeholder="Enter your username" variant="outline" required={true} />
+                <Input type="password" name="password" placeholder="Enter your password" variant="outline" required={true} />
                 <BaseButton label="login" type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" /> 
             </form>
         </div>
---
+++ app/feed/page.tsx
@@ -0,0 +1,12 @@
+
+
+const FeedPage = () => {
+    return (
+        <div>
+        <h1>Feed Page</h1>
+        <p>This is the feed page content.</p>
+        </div>
+    );
+}
+
+export default FeedPage;
