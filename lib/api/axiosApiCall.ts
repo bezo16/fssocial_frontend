525cd55e99b8172f880fa6de1368349e9cc69aa8
bezo <bezo16@protonmail.ch>
2025-07-25 13:46:32 +0200

    feat: Authorization header in axiosApiCall

--- lib/api/axiosApiCall.ts
+++ lib/api/axiosApiCall.ts
@@ -5,4 +5,14 @@ const axiosApiCall: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
 
+axiosApiCall.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const authToken = localStorage.getItem("authToken")
+    if (authToken) {
+      config.headers["Authorization"] = authToken
+    }
+  }
+  return config
+})
+
 export default axiosApiCall
